d273b30e9defe6dedbd0526a7daf262bb07bb0c8
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-02-02 17:42:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -38,4 +38,36 @@ function repeat(greeting, num) {
 }
 repeat("Hi", 3)
 
+
+// let arr = [];
+// let newItem;
+// let deleteItem;
+// let whatToDo;
+// while (true) {
+//     whatToDo = prompt("What would you like to do ?");
+//     if (whatToDo === "new") {
+//         newItem = prompt("Enter a new Todo");
+//         arr.push(newItem)
+//         console.log(`${newItem} added to list`)
+//     }
+    
+//     if (whatToDo === "delete") {
+//         deleteItem = prompt("What item you want to delete?") - 1;
+//         if(Number.isInteger(parseInt(deleteItem)) === true) {
+//         arr.splice(deleteItem, 1);
+//         console.log(`${deleteItem + 1} item deleted`);
+//     }
+//     }   
+//     else if (whatToDo === "list") {
+//         console.log("**********")
+//         for (let i = 0; i < arr.length; i++) {
+//             console.log(`${i+1}: ${arr[i]}`)  
+//         }
+//         console.log("**********")
+//     }
+//     else if(whatToDo === "quit"){
+//         console.log("OK YOU QUIT THE APP!"); break;
+//     }
+// }
+
      
