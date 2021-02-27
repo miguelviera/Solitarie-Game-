c262a0eebc55cbf2a61d797f624a34a9a822eaf3
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-02-27 10:01:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -46,15 +46,6 @@ button.addEventListener("click", function () {
 //     h1.innerText = randomColor;
 // })
 
-// function changeColor() {
-//         let color = "";
-//     for(let i = 0; i < 3; i++) {
-//         let sub = Math.floor(Math.random() * 256).toString(16);
-//         color += (sub.length == 1 ? "0" + sub : sub);
-//     }
-//     return "#" + color;
-//     }
-
 
 
 
