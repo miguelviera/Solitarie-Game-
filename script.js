0c43f677f4ccf8948fb3a8e36bd679fd61d14857
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-03-04 13:53:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const body = document.querySelector("body");
 const button = document.querySelector("button");
 const h1 = document.querySelector("h1");
 
-
+body.style.textAlign = "center";
 
 button.addEventListener("click", function () {
     const arrColor = [];
